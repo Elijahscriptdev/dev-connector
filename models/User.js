b5fa8a55f30ebb3efa265649486ec3a01d725dd1
Elijahscriptdev <elijahobominuru@gmail.com>
2020-10-05 18:50:22 +0100

    add users model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongosse = require('mongoose');
+
+const UserSchema = new mongosse.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongosse.model('user', UserSchema);
