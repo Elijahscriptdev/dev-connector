2235e106455555e13b74f3d361a2144aaa5c2208
Elijahscriptdev <elijahobominuru@gmail.com>
2020-10-05 20:32:00 +0100

    add validation to users model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
-// route =  GET api/users
-// desc  =  LIST all users
+// route =  POST api/users
+// desc  =  REGISTER user
 // access = public
-router.get('/', (req, res) => {
-    res.send('User Route')
-})
+router.post(
+  "/",
+  [
+    check("name", "Name is Required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User Route");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const express = require('express')
+const express = require('express');
+const { body } = require('express-validator');
 const connectDB = require('./config/db')
 
 const app = express();
 
 // Connect DB
 connectDB()
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'))
 
