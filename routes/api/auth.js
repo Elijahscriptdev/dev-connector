6066a05c548d21af43760f095f566db75d7c3ba7
Elijahscriptdev <elijahobominuru@gmail.com>
2020-10-07 00:44:11 +0100

    add custom middleware and JWT verify

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth"); //middleware validates token
+const User = require("../../models/User");
 
 // route =  GET api/auth
 // desc  =  auth
 // access = public
-router.get('/', (req, res) => {
-    res.send('Auth Route')
-})
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server Error");
+  }
+  res.send(req.user);
+});
 
 module.exports = router;
