93b748a676f4ecdba614d8f14b49cf412299041a
Elijahscriptdev <elijahobominuru@gmail.com>
2020-10-10 20:09:13 +0100

    setup react-router for signup and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import React, { Fragment } from 'react';
-import './App.css';
-import Landing from './components/layout/Landing';
-import Navbar from './components/layout/Navbar';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar";
 
-const App = () => 
+
+const App = () => (
+  <Router>
     <Fragment>
       <Navbar />
-      <Landing />
+      <Route exact path='/' component={Landing} />
+      <section className="container">
+        <Switch>
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        </Switch>
+      </section>
     </Fragment>
-
+  </Router>
+);
 export default App;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,23 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className='navbar bg-dark'>
       <h1>
-        <a href='index.html'>
-          <i className='fas fa-code'></i> DevConnector
-        </a>
+        <Link to='/'>
+          <i className='fas fa-code' />
+          DevConnector
+        </Link>
       </h1>
       <ul>
-        <li>
-          <a href='profiles.html'>Developers</a>
-        </li>
-        <li>
-          <a href='register.html'>Register</a>
-        </li>
-        <li>
-          <a href='login.html'>Login</a>
-        </li>
+        <Link to="!#">
+          Developers
+        </Link>
+        <Link to="/register">
+          Register
+        </Link>
+        <Link to="/login">
+          Login
+        </Link>
       </ul>
     </nav>
   );
